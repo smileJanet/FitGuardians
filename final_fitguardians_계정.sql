0e91d1bb0ef9c2429f071251553455edc71ed0c2
kimsssang <swk9871@gmail.com>
2024-10-09 17:48:42 +0900

    sql

--- final_fitguardians_계정.sql
+++ final_fitguardians_계정.sql
@@ -1,11 +1,14 @@
 INSERT INTO member (user_no, user_id, user_pwd, user_name, email, phone, gender, age, address, enroll_date, user_level, pt, status, api, qr, profile_pic, membership)
 VALUES
-(1, 'user01', 'Password!1', '김민수', 'minsu.kim@example.com', '01012345678', 'M', 25, '서울시 강남구 123', SYSDATE, 1, 'Trainer A', 'Y', 'api_key_1', 'qr_code_path_1', 'profile_pic_path_1', 'Gold Membership'),
+(seq_mno.nextval, 'user01', 'Password!1', '김민수', 'minsu.kim@example.com', '01012345678', 'M', 25, '서울시 강남구 123', SYSDATE, 1, 'Trainer A', 'Y', 'api_key_1', 'qr_code_path_1', 'profile_pic_path_1', 'Gold Membership');
+INSERT INTO member (user_no, user_id, user_pwd, user_name, email, phone, gender, age, address, enroll_date, user_level, pt, status, api, qr, profile_pic, membership)
+VALUES
+(seq_mno.nextval, 'user02', 'SecurePassword2!', '이수진', 'sujin.lee@example.com', '01098765432', 'F', 30, '부산시 해운대구 456', SYSDATE, 2, 'Trainer B', 'Y', 'api_key_2', 'qr_code_path_2', 'profile_pic_path_2', 'Silver Membership');
+INSERT INTO member (user_no, user_id, user_pwd, user_name, email, phone, gender, age, address, enroll_date, user_level, pt, status, api, qr, profile_pic, membership)
+VALUES
+(seq_mno.nextval, 'user03', 'StrongPwd3$', '박지현', 'jihyun.park@example.com', '01045678910', 'F', 28, '대전시 유성구 789', SYSDATE, 2, 'Trainer C', 'Y', 'api_key_3', 'qr_code_path_3', 'profile_pic_path_3', 'Platinum Membership');
 
-(2, 'user02', 'SecurePassword2!', '이수진', 'sujin.lee@example.com', '01098765432', 'F', 30, '부산시 해운대구 456', SYSDATE, 2, 'Trainer B', 'Y', 'api_key_2', 'qr_code_path_2', 'profile_pic_path_2', 'Silver Membership'),
-
-(3, 'user03', 'StrongPwd3$', '박지현', 'jihyun.park@example.com', '01045678910', 'F', 28, '대전시 유성구 789', SYSDATE, 2, 'Trainer C', 'Y', 'api_key_3', 'qr_code_path_3', 'profile_pic_path_3', 'Platinum Membership');
-
+commit;
 
 INSERT 
 		  INTO member 
--- fitguardians스트립트.sql
+++ fitguardians스트립트.sql
@@ -19,7 +19,7 @@ CREATE TABLE member (
     attendance varchar2(100)   
 );
 drop SEQUENCE seq_mno;
-create sequence seq_mno start with 4 NOCACHE;
+create sequence seq_mno NOCACHE;
 
 COMMENT ON COLUMN member.user_no IS '회원번호';
 COMMENT ON COLUMN member.user_id IS '회원아이디';
@@ -40,6 +40,18 @@ COMMENT ON COLUMN member.profile_pic IS '회원 프로필 사진';
 COMMENT ON COLUMN member.membership IS '회원이 소유한 회원권';
 comment on column member.attendance is '출결';
 
+-- member samlpe 
+INSERT INTO member (user_no, user_id, user_pwd, user_name, email, phone, gender, age, address, enroll_date, user_level, pt, status, api, qr, profile_pic, membership)
+VALUES
+(seq_mno.nextval, 'user01', 'Password!1', '김민수', 'minsu.kim@example.com', '01012345678', 'M', 25, '서울시 강남구 123', SYSDATE, 1, 'Trainer A', 'Y', 'api_key_1', 'qr_code_path_1', 'profile_pic_path_1', 'Gold Membership');
+INSERT INTO member (user_no, user_id, user_pwd, user_name, email, phone, gender, age, address, enroll_date, user_level, pt, status, api, qr, profile_pic, membership)
+VALUES
+(seq_mno.nextval, 'user02', 'SecurePassword2!', '이수진', 'sujin.lee@example.com', '01098765432', 'F', 30, '부산시 해운대구 456', SYSDATE, 2, 'Trainer B', 'Y', 'api_key_2', 'qr_code_path_2', 'profile_pic_path_2', 'Silver Membership');
+INSERT INTO member (user_no, user_id, user_pwd, user_name, email, phone, gender, age, address, enroll_date, user_level, pt, status, api, qr, profile_pic, membership)
+VALUES
+(seq_mno.nextval, 'user03', 'StrongPwd3$', '박지현', 'jihyun.park@example.com', '01045678910', 'F', 28, '대전시 유성구 789', SYSDATE, 2, 'Trainer C', 'Y', 'api_key_3', 'qr_code_path_3', 'profile_pic_path_3', 'Platinum Membership');
+
+commit;
 -- schedule table --
 drop table schedule;
 CREATE TABLE SCHEDULE (
@@ -55,6 +67,16 @@ CREATE TABLE SCHEDULE (
 );
 drop sequence seq_cno;
 create sequence seq_cno nocache;
+
+COMMENT ON COLUMN SCHEDULE.SCHEDULE_NO IS '스케줄 고유번호';
+COMMENT ON COLUMN SCHEDULE.USER_NO IS '스케줄 보유회원 아이디';
+COMMENT ON COLUMN SCHEDULE.SCHEDULE_TITLE IS '스케줄 이름';
+COMMENT ON COLUMN SCHEDULE.SCHEDULE_DES IS '스케줄 내용';
+COMMENT ON COLUMN SCHEDULE.START_DATE IS '시작 날짜';
+COMMENT ON COLUMN SCHEDULE.END_DATE IS '종료 날짜';
+COMMENT ON COLUMN SCHEDULE.DOW IS '요일';
+COMMENT ON COLUMN SCHEDULE.BACK_COLOR IS '배경색';
+COMMENT ON COLUMN SCHEDULE.ALIDAY IS '종일 일정 여부';
 
 drop table qrInfo;
 create table qrInfo (
@@ -72,3 +94,11 @@ create table qrInfo (
 drop sequence seq_qno;
 create sequence seq_qno nocache;
 
+COMMENT ON COLUMN qrInfo.qr_no IS 'QR 코드 고유 번호';
+COMMENT ON COLUMN qrInfo.id IS '사용자 ID';
+COMMENT ON COLUMN qrInfo.type IS '사용자 타입';
+COMMENT ON COLUMN qrInfo.created_at IS '생성 일시';
+COMMENT ON COLUMN qrInfo.valid_until IS '유효 기간';
+COMMENT ON COLUMN qrInfo.attendance IS '출석 정보';
+COMMENT ON COLUMN qrInfo.status IS '유효 여부 (기본값: Y)';
+COMMENT ON COLUMN qrInfo.att_status IS '출결 상태';
